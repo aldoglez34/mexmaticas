41af91c19586256fdb67ab600fb4d2473d75a93c
Aldo Solano <aldoglez34@gmail.com>
2022-03-09 11:35:25 -0600

    new grades texts

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "7.2";
+export const APP_VERSION = "7.3";
 
 export const IMAGES = {
   SIZE: 1000000,
--- client/src/pages/courseinfo/CourseInfoPage.jsx
+++ client/src/pages/courseinfo/CourseInfoPage.jsx
@@ -32,15 +32,15 @@ export const CourseInfoPage = React.memo((props) => {
   const setDescription = () => {
     switch (school) {
       case "Primaria":
-        return "Compuesto por las bases de la Aritmética este curso es ideal para que los alumnos desde 3er grado hasta 6to desarrollen y practiquen sus habilidades lógico-matemáticas, así como para los estudiantes        que necesiten fortalecer sus bases en Aritmética puedan disponer del curso en cualquier momento del día.";
+        return "Compuesto por las bases de la aritmética este curso es ideal para que los alumnos desde primero hasta sexto grado desarrollen y practiquen sus habilidades lógico-matemáticas, así como para los estudiantes que necesiten fortalecer sus bases en aritmética puedan disponer del curso en cualquier momento del día.";
       case "Secundaria":
-        return "La secundaria es una de las etapas más importantes de la vida ya que es donde empieza la transición de niño a adulto; las matemáticas pueden parecer igual de confusas para un adolescente, sin embargo este curso ha sido diseñado de una forma tal que el alumno aprenda y refuerce los conocimientos básicos del Álgebra y esté preparado para enfrentar los cursos posteriores en preparatoria.";
+        return "La secundaria es una de las etapas más importantes de la vida ya que es donde empieza la transición de niño a adulto; las matemáticas pueden parecer igual de confusas para un adolescente, sin embargo este curso ha sido diseñado de una forma tal que el alumno aprenda y refuerce los conocimientos básicos del álgebra y esté preparado para enfrentar los cursos posteriores en preparatoria.";
       case "Preparatoria":
-        return "Independientemente de la carrera por la cual se llegue a decidir, la Trigonometría, Álgebra y Estadística son de los retos que el estudiante en Preparatoria puede llegar a enfrentar, el curso ha sido pensado para acompañar, practicar y aprender estos temas que lo ayudarán a prepararse de tal forma que en su vida universitaria el alumno pueda llevar una vida equilibrada entre asignaciones.";
+        return "Independientemente de la carrera por la cual se llegue a decidir, la trigonometría, la geometría y el álgebra son de los retos que el estudiante en Preparatoria puede llegar a enfrentar, el curso ha sido pensado para acompañar, practicar y aprender estos temas que lo ayudarán a prepararse de tal forma que en su vida universitaria el alumno pueda llevar una vida equilibrada entre asignaciones.";
       case "Universidad":
-        return "lorem ipsum";
+        return "Estos cursos están diseñados para sumergir al alumno que haya decidido tomar alguna carrera del área físico-matemáticas en el cálculo diferencial, integral y álgebra lineal para que así durante su vida estudiantil pueda enfrentar con bases sólidas los temas más complejos que tienen como raíz de todo estos temas antes mencionados.";
       default:
-        return null;
+        return "";
     }
   };
 
